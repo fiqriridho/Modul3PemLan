4261bcc96bc725485036556f882ef046d8b76de6
fiqriridho <fiqri6149@gmail.com>
2025-10-24 16:22:08 +0700

    Mengganti File main

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Modul3PemLan
+# Codelab Modul3PemLan
+# Documentation Practice
+
+## 📌 Deskripsi
+Proyek ini dibuat untuk latihan dokumentasi pada Modul 3 Pemrograman Lanjut.  
+Program sederhana ini menampilkan pesan ke layar sebagai contoh penggunaan Java.
+
+## 🧩 Cara Menjalankan
+1. Clone repository ini:
+   ```bash
+   git clone https://github.com/fiqriridho/PemLan_E_Fiqri_167.git
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Halo Kawan!");
-        System.out.println("Kamu Keren ");
-        System.out.println("Jangan Menyerah!!!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah nilai: ");
+        int jumlah = input.nextInt();
+
+        double[] nilai = new double[jumlah];
+        double total = 0;
+
+        for (int i = 0; i < jumlah; i++) {
+            System.out.print("Masukkan nilai ke-" + (i + 1) + ": ");
+            nilai[i] = input.nextDouble();
+            total += nilai[i];
+        }
+
+        double rataRata = total / jumlah;
+
+        System.out.println("\nHasil Perhitungan");
+        for (int i = 0; i < jumlah; i++) {
+            System.out.println("Nilai ke-" + (i + 1) + ": " + nilai[i]);
+        }
+        if (rataRata <= 60){
+            System.out.println("\nBelajar lagi dek");
+        }else if (rataRata <=79){
+            System.out.println("\nWow kamu sedikit keren, belajar lebih giat yaa! ");
+        }else {
+            System.out.println("\nKAMU KEREN BRO!!!!");
+        }
+
+        System.out.println("\nRata-rata   : " + rataRata);
+
+        input.close();
     }
 }
