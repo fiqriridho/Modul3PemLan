6fcef65f9d28fc8ea9b9eb65213617d56b251d7a
fiqriridho <fiqri6149@gmail.com>
2025-10-24 16:05:44 +0700

    Tambah Kata-kata semangat di line 5

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Halo Kawan!");
         System.out.println("Kamu Keren ");
+        System.out.println("Jangan Menyerah!!!");
     }
 }
